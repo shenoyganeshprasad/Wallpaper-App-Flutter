f68642a30914b5e8cc1793980b693887ed009413
Ganeshprasad Shenoy <ganesh@Ganeshprasads-MacBook-Air.local>
2020-02-03 20:17:03 +0530

    initial commit

---
+++ lib/wallpapers.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Wallpaper extends StatefulWidget {
+  @override
+  _WallpaperState createState() => _WallpaperState();
+}
+
+class _WallpaperState extends State<Wallpaper> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Wallpapers Here'),
+    );
+  }
+}
